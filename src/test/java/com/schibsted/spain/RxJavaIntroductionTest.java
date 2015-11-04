cbc05498fa89b21f6d1d8948fa9506ad95cea380
Bernat Borrás Paronella <bernat.borras@scmspain.com>
2015-11-04 10:30:40 +0100

    Test now()

--- src/test/java/com/schibsted/spain/RxJavaIntroductionTest.java
+++ src/test/java/com/schibsted/spain/RxJavaIntroductionTest.java
@@ -2,6 +2,9 @@ package com.schibsted.spain;
 
 import org.junit.Test;
 import rx.Observable;
+import rx.Observer;
+
+import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,5 +17,27 @@ public class RxJavaIntroductionTest {
         .toBlocking().single();
 
     assertEquals(3,value);
+  }
+
+  @Test
+  public void testNowObservable() throws Exception{
+    Observer<Date> observer = new Observer<Date>() {
+      @Override
+      public void onCompleted() {
+          System.out.println("onComplete");
+      }
+
+      @Override
+      public void onError(Throwable e) {
+        e.printStackTrace();
+      }
+
+      @Override
+      public void onNext(Date date) {
+        System.out.println(date);
+      }
+    };
+
+    Observable.just(new Date()).subscribe(observer);
   }
 }
