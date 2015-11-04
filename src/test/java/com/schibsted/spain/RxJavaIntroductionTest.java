e7783995d147faf63c94a3349d261578dc9b2bed
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-11-04 11:45:20 +0100

    INtervals with defer()

--- src/test/java/com/schibsted/spain/RxJavaIntroductionTest.java
+++ src/test/java/com/schibsted/spain/RxJavaIntroductionTest.java
@@ -3,6 +3,8 @@ package com.schibsted.spain;
 import org.junit.Test;
 import rx.Observable;
 import rx.Observer;
+import rx.Subscriber;
+import rx.functions.Func1;
 import rx.observers.TestSubscriber;
 
 import java.util.Date;
@@ -44,6 +46,39 @@ public class RxJavaIntroductionTest {
     }
 
     @Test
+    public void testDatesObservable() throws Exception {
+        Observer<Date> observer = new Observer<Date>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("onComplete");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onNext(Date date) {
+                System.out.println(date);
+            }
+        };
+
+        Observable<Date> observable = Observable.defer(() -> Observable.just(new Date()));
+
+        Observable<Date> dateObservable = Observable.interval(1, TimeUnit.SECONDS)
+                .take(4)
+                .flatMap(aLong -> observable);
+
+
+        TestSubscriber<Date> testSubscriber = new TestSubscriber<>(observer);
+        dateObservable.subscribe(testSubscriber);
+
+        testSubscriber.awaitTerminalEvent();
+
+    }
+
+    @Test
     public void testIntervalObservable() throws InterruptedException {
         Observable<Long> observable = Observable.interval(1, TimeUnit.SECONDS).take(6);
 
@@ -78,4 +113,6 @@ public class RxJavaIntroductionTest {
         Observable.just("Patata").take(4).subscribe(System.out::println);
         Observable.just("Patata", "Jamon", "Aceitunas", "Ensaladilla russa", "Berberechos").take(4).subscribe(System.out::println);
     }
+
+
 }
